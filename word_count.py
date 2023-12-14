f052034d5fb52f44679a435f50a7a143a6771d04
Andrew Liew Weida <activesun1982@gmail.com>
2023-12-14 18:39:00 +0800

    add the ability to read the file path and file name and run word count, also modify word count package.

--- word_count.py
+++ word_count.py
@@ -1,56 +1,45 @@
 #!/usr/bin/env python3
 
-import os
 import sys
-import re
-import time
-import PyPDF2
+import os
+from PyPDF2 import PdfReader
 
 def getPageCount(pdf_file):
-
-	pdfFileObj = open(pdf_file, 'rb')
-	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-	pages = pdfReader.numPages
-	return pages
+    pdfFileObj = open(pdf_file, 'rb')
+    pdfReader = PdfReader(pdfFileObj)
+    pages = len(pdfReader.pages)
+    return pages
 
 def extractData(pdf_file, page):
-
-	pdfFileObj = open(pdf_file, 'rb')
-	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-	pageObj = pdfReader.getPage(page)
-	data = pageObj.extractText()
-	return data
+    pdfFileObj = open(pdf_file, 'rb')
+    pdfReader = PdfReader(pdfFileObj)
+    pageObj = pdfReader.pages[page]
+    data = pageObj.extract_text()
+    return data
 
 def getWordCount(data):
-
-	data=data.split()
-	return len(data)
+    data = data.split()
+    return len(data)
 
 def main():
-	if len(sys.argv)!=2:
-		print('command usage: python word_count.py FileName')
-		exit(1)
-	else:
-		pdfFile = sys.argv[1]
-	
-		# check if the specified file exists or not
-		try:
-			if os.path.exists(pdfFile):
-				print("file found!")
-		except OSError as err:
-			print(err.reason)
-			exit(1)
+    if len(sys.argv) != 2:
+        print('command usage: python word_count.py FileName')
+        exit(1)
+    else:
+        pdfFile = sys.argv[1]
 
+        # Check if the specified file exists or not
+        if os.path.exists(pdfFile):
+            print("file found!")
 
-		# get the word count in the pdf file
-		totalWords = 0
-		numPages = getPageCount(pdfFile)
-		for i in range(numPages):
-			text = extractData(pdfFile, i)
-			totalWords+=getWordCount(text)
-		time.sleep(1)
+        # Get the word count in the PDF file
+        totalWords = 0
+        numPages = getPageCount(pdfFile)
+        for i in range(numPages):
+            text = extractData(pdfFile, i)
+            totalWords += getWordCount(text)
 
-		print (totalWords)
+        print(totalWords)
 
 if __name__ == '__main__':
-	main()
+    main()
